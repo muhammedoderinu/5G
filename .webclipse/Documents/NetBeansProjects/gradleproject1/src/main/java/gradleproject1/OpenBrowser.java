9595751b97e5432dc29a603b3efeb435520ee64c
muhammed <muhammedoderinu@gmail.com>
2021-07-22 23:14:34 -0800

    this is a demo code written to raise 5 view for now

--- .webclipse/Documents/NetBeansProjects/gradleproject1/src/main/java/gradleproject1/OpenBrowser.java
+++ .webclipse/Documents/NetBeansProjects/gradleproject1/src/main/java/gradleproject1/OpenBrowser.java
@@ -9,6 +9,7 @@ import javax.swing.JOptionPane;
 import java.net.*;
 import java.util.*;
 import java.io.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -16,32 +17,34 @@ import java.io.*;
  */
 public class OpenBrowser {
     
-     public static void openURL(String url) {
+     public static void openURL(String url, String proxy) {
 		String osName = System.getProperty("os.name");
                 ArrayList<String> proxies = new ArrayList<>();
-                proxies.add("168.228.224.68");
-                proxies.add("176.65.252.182");
-                proxies.add("178.18.34.26");
-                proxies.add("95.38.214.32");
-                proxies.add("95.107.173.35");
-                for (int i=0; i<proxies.size(); i++){
+                Process p;
                 
 		try {
-                    //Proxy proxy =new Proxy(Proxy.Type.HTTP, new InetSocketAddress("123.0.0.1",8080));
-                    //URL urls = new URL(url);
-                    //HttpURLConnection uc = (HttpURLConnection)urls.openConnection(proxy);
-                   
-                    System.setProperty("Http.proxyHost",proxies.get(i));
-			if (osName.startsWith("Windows"))
-                            
-				Runtime.getRuntime().exec(
+                    
+                   if(!proxy.isEmpty()){
+                     System.getProperties().put("Http.proxyHost",proxy);
+                       }
+			if (osName.startsWith("Windows")){
+                            p= Runtime.getRuntime().exec(
 						"rundll32 url.dll,FileProtocolHandler " + url);
-                              
-
-                              
-                                //uc.connect();
-			else {
-				String[] browsers = { "firefox", "opera", "konqueror",
+                          for(int i=0;i<15;i++){
+                              Proxy prox = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxy, 8080));
+                             URLConnection conn = new URL(url).openConnection(prox);
+                         //URL myURL = new URL(url);
+                      //URLConnection myURLConnection = myURL.openConnection();
+                        //try (InputStream is = myURLConnection.getInputStream()) {}
+                            }
+                            
+			
+                        TimeUnit.SECONDS.sleep(30);
+                        p.destroyForcibly();
+                                    
+                        
+                        }else {
+				String[] browsers = { "firefox","chrome", "opera", "konqueror",
 						"epiphany", "mozilla", "netscape" };
 				String browser = null;
 				for (int count = 0; count < browsers.length && browser == null; count++)
@@ -50,19 +53,26 @@ public class OpenBrowser {
 							.waitFor() == 0)
 						browser = browsers[count];
                                 
-				Runtime.getRuntime().exec(new String[] { browser, url });
-                                
+				p = Runtime.getRuntime().exec(new String[] { browser, url });
+                                p.destroyForcibly();
+                                p.exitValue();
+                                p.destroy();
                                 //Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("123.0.0.1",8080));
                                 //URL urls = new URL(url);
                                // HttpURLConnection uc = (HttpURLConnection)urls.openConnection(proxy);
                                // uc.connect();
+
 			}
+                        
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Error in opening browser"
 					+ ":\n" + e.getLocalizedMessage());
 		}
                 
-                }
+                
+                
+              
+               
 	}
     
 }
